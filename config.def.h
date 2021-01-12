614e4f3dc9177405f998b60bb7d746d9c3bd3a1a
Mathias Maisberger <me@hiasinho.com>
2021-01-12 08:27:00 +0100

    Fix resizehints for st
    https://wiki.archlinux.org/index.php/dwm#Fixing_gaps_around_terminal_windows

--- config.def.h
+++ config.def.h
@@ -37,12 +37,13 @@ static const Rule rules[] = {
 	{ "St",       NULL,       "newsboat", 0,            1,           1,           -1 },
 	{ "Arandr",   NULL,       NULL,       0,            1,           1,           -1 },
 	{ "zoom",     NULL,       "Settings", 0,            1,           1,           -1 },
+	{ "Xscreensaver-demo", NULL, NULL,    0,            1,           1,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static int attachbelow = 1;          /* 1 means attach after the currently active window */
 
 static const Layout layouts[] = {
@@ -50,6 +51,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[D]",      deck },
 };
 
 /* key definitions */
@@ -89,6 +91,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
